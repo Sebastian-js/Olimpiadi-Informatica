12b9847d1896d99bd054bef7591af9fe15947793
Sebastian-js <sebastian.jusca@liceonewton.edu.it>
2023-11-08 17:31:32 +0100

    god only knows

--- Fire on a Tree (fire)/fire.c
+++ Fire on a Tree (fire)/fire.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // constraints
 #define MAXN 200000
@@ -12,19 +13,68 @@ int N;
 int A[MAXN];
 int B[MAXN];
 
+int funzionechemirestituiscequellomaggiore(int kurva, int max, int* posmax, int j){
+  if(kurva>max) {
+    *posmax = j;
+    return kurva;
+  }
+  return max;
+}
 int main() {
   // uncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
+  freopen("input.txt", "r", stdin);
+  int a, b, i, j, max=0;
   // freopen("output.txt", "w", stdout);
 
   assert(1 == scanf("%d", &N));
-  for (int i = 0; i < N - 1; i++) {
-    assert(2 == scanf("%d%d", &A[i], &B[i]));
+  int connessioni[N][N];
+
+  for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      connessioni[i][j] = 0;
+    }
+  }
+  int occorrenze[N];
+  for (i = 0; i < N - 1; i++) {
+    scanf("%d", &a);
+    scanf("%d", &b);
+    connessioni[b][a]=1;
   }
 
-  // insert your code here
+  int sum=0;
+  for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      sum+=connessioni[j][i];
+    }
+    occorrenze[i]=sum;
+    sum=0;
+  }
 
-  printf("%d\n", 42);  // print the result
+  for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      printf("%d ", connessioni[i][j]);
+    }
+    printf("\n");
+  }
+  for(i=0; i<N; i++){
+    printf("%d ", occorrenze[i]+1);
+  }
 
+  /*
+
+  int posmax = 0;
+   for(i=0; i<N; i++){
+    for(j=0; j<N; j++){
+      if(connessioni[i][j]==1){
+        max=funzionechemirestituiscequellomaggiore(occorrenze[j],max, &posmax, j);
+      }
+    }
+
+  }
+
+  printf("\n%d %d", max, posmax);
+  // printf("%d\n", 42); print the result
+  
+  */
   return 0;
 }
